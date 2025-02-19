9f7633339aad5643693d57b697972c249bae8680
MateusRodr <mr5508113@gmail.com>
2025-02-19 08:04:13 -0300

    removendo build do vercel

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist:true, forbidNonWhitelisted:true}))
   app.use(cors({ origin: '*' }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
